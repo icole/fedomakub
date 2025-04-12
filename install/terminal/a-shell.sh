20392819055fff45f8cc35b9290255e41c89332c
Josh Voyles <120749218+Josh-Voyles@users.noreply.github.com>
2025-04-12 08:25:41 -0400

    updated terminal apps Fedora version/commands

--- install/terminal/a-shell.sh
+++ install/terminal/a-shell.sh
@@ -1,10 +1,10 @@
-# Configure the bash shell using Omakub defaults
+# Configure the bash shell using Fedomakub defaults
 [ -f "~/.bashrc" ] && mv ~/.bashrc ~/.bashrc.bak
-cp ~/.local/share/omakub/configs/bashrc ~/.bashrc
+cp ~/.local/share/fedomakub/configs/bashrc ~/.bashrc
 
 # Load the PATH for use later in the installers
-source ~/.local/share/omakub/defaults/bash/shell
+source ~/.local/share/fedomakub/defaults/bash/shell
 
 [ -f "~/.inputrc" ] && mv ~/.inputrc ~/.inputrc.bak
-# Configure the inputrc using Omakub defaults
-cp ~/.local/share/omakub/configs/inputrc ~/.inputrc
+# Configure the inputrc using Fedomakub defaults
+cp ~/.local/share/fedomakub/configs/inputrc ~/.inputrc
--- install/terminal/docker.sh
+++ install/terminal/docker.sh
@@ -1,12 +1,5 @@
-# Add the official Docker repo
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo wget -qO /etc/apt/keyrings/docker.asc https://download.docker.com/linux/ubuntu/gpg
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-
 # Install Docker engine and standard plugins
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+sudo dnf install -y moby-engine containerd docker-buildx docker-cli docker-compose docker-compose-switch
 
 # Give this user privileged Docker access
 sudo usermod -aG docker ${USER}
