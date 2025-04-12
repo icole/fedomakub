50458b9feea8c069ad4f898158de14b78b258728
Josh Voyles <120749218+Josh-Voyles@users.noreply.github.com>
2025-04-12 15:50:57 -0400

    updated paths, readme thanks, version

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 set -e
 
 # Check the distribution name and version and abort if incompatible
-source ~/.local/share/omakub/install/check-version.sh
+source ~/.local/share/fedomakub/install/check-version.sh
 
 # Ask for app choices
 echo "Get ready to make a few choices..."
-source ~/.local/share/omakub/install/terminal/required/app-gum.sh >/dev/null
-source ~/.local/share/omakub/install/first-run-choices.sh
+source ~/.local/share/fedomakub/install/terminal/required/app-gum.sh >/dev/null
+source ~/.local/share/fedomakub/install/first-run-choices.sh
 
 # Desktop software and tweaks will only be installed if we're running Gnome
 if [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]]; then
@@ -18,15 +18,15 @@ if [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]]; then
   echo "Installing terminal and desktop tools..."
 
   # Install terminal tools
-  source ~/.local/share/omakub/install/terminal.sh
+  source ~/.local/share/fedomakub/install/terminal.sh
 
   # Install desktop tools and tweaks
-  source ~/.local/share/omakub/install/desktop.sh
+  source ~/.local/share/fedomakub/install/desktop.sh
 
   # Revert to normal idle and lock settings
   gsettings set org.gnome.desktop.screensaver lock-enabled true
   gsettings set org.gnome.desktop.session idle-delay 300
 else
   echo "Only installing terminal tools..."
-  source ~/.local/share/omakub/install/terminal.sh
+  source ~/.local/share/fedomakub/install/terminal.sh
 fi
