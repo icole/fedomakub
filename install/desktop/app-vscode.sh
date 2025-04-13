10b45c8a6a635aebac579eac289e4fab2af623d3
Josh Voyles <120749218+Josh-Voyles@users.noreply.github.com>
2025-04-13 09:20:15 -0400

    ensured docker daemon running, add vscode repo for easy PATH

--- install/desktop/app-vscode.sh
+++ install/desktop/app-vscode.sh
@@ -1,5 +1,10 @@
 # eventually want to replace with non-privacy-invasive-vscode
-flatpak install -y flathub com.visualstudio.code
+# Add the Microsoft Visual Studio Code repository
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+# Install Visual Studio Code
+sudo dnf install -y code
 
 mkdir -p ~/.config/Code/User
 cp ~/.local/share/fedomakub/configs/vscode.json ~/.config/Code/User/settings.json
